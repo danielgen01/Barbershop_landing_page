0737251891b22522ba52951a9e3d276259671c48
Daniel <gendik38@gmail.com>
2023-03-22 09:19:41 +0100

    add correct font, delete old fonts,add favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,25 +7,25 @@ import {HiMenuAlt3} from "react-icons/hi"
 const Navbar:React.FC = () => {
   return (
     
-    <nav className="flex justify-between items-center h-40 mx-10">
+    <nav className="flex justify-between items-center h-40 mx-10 text-2xl">
         <img src={logo} alt="logo" className=""/>
 
         <div className="links text-white  items-center gap-5 font-medium text-md hidden xl:flex">
-            <a href="#" className="uppercase text-barberColor">the home</a>
-            <a href="#" className="uppercase">services and prices</a>
-            <a href="#" className="uppercase">acedemy</a>
-            <a href="#" className="uppercase">franchise</a>
-            <a href="#" className="uppercase">our team</a>
-            <a href="#" className="uppercase">contact</a>
-            <a href="#" className="uppercase">portfolio</a>
-            <a href="#" className="uppercase">shop</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase text-barberColor">the home</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">services and prices</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">acedemy</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">franchise</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">our team</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">contact</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">portfolio</a>
+            <a href="#" className="hover:text-barberColor duration-200 uppercase">shop</a>
 
 
         </div>
 
         <div className="cart flex items-center">
             <BiShoppingBag className="text-4xl text-white"/>
-            <p className="bg-barberColor px-2 rounded-full text-white font-bold -ml-5 -mt-5">0</p>
+            <p className="bg-barberColor px-2 text-sm rounded-full text-white font-semibold -ml-5 -mt-5">0</p>
         </div>
         <HiMenuAlt3 className="text-gray-300 text-5xl xl:hidden"/>
     </nav>
